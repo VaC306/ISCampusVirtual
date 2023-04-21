8429219525fc392dd4b0391c5461fee19147c75a
VaC306 <rishipursnani@gmail.com>
2023-04-21 12:53:04 +0200

    perfil y perfil añadir cambios, hay mas

---
+++ ISCampusVirtual/src/Presentacion/VPerfilEditar.java
@@ -0,0 +1,11 @@
+package Presentacion;
+
+import Negocio.Usuario.TransferUsuario;
+
+public class VPerfilEditar extends VPerfil{
+
+	public VPerfilEditar(TransferUsuario data) {
+		super(data);
+	}
+
+}
