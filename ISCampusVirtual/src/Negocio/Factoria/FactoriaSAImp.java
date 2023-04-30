d93183a6743603c4109d1bf9a9a437fa86aef43a
VaC306 <rishipursnani@gmail.com>
2023-04-30 22:19:23 +0200

    cambios en factoria sa y imp

--- ISCampusVirtual/src/Negocio/Factoria/FactoriaSAImp.java
+++ ISCampusVirtual/src/Negocio/Factoria/FactoriaSAImp.java
@@ -1,11 +1,61 @@
 package Negocio.Factoria;
 
 import Negocio.Aula.SAAsignatura;
+import Negocio.Aula.SAClase;
+import Negocio.Aula.SACurso;
+import Negocio.Aula.SATema;
+import Negocio.Foro.SAMensaje;
+import Negocio.Usuario.SAAlumno;
+import Negocio.Usuario.SAProfesor;
+import Negocio.Usuario.SAUsuario;
 
 public class FactoriaSAImp extends FactoriaSA {
 	
+	@Override
 	public SAAsignatura generarSAAsignatura() {
 		
 		return new SAAsignatura();
+	}
+
+	@Override
+	public SAClase generarSAAClase() {
+		
+		return new SAClase();
+	}
+
+	@Override
+	public SACurso generarSACurso() {
+		
+		return new SACurso();
+	}
+
+	@Override
+	public SATema generarSATema() {
+		
+		return new SATema();
+	}
+
+	@Override
+	public SAMensaje generarSAMensaje() {
+		
+		return new SAMensaje();
+	}
+
+	@Override
+	public SAAlumno generarSAAlumno() {
+		
+		return new SAAlumno();
+	}
+
+	@Override
+	public SAProfesor generarSAProfesor() {
+		
+		return new SAProfesor();
+	}
+
+	@Override
+	public SAUsuario generarSAUsuario() {
+		
+		return new SAUsuario();
 	}
 }
