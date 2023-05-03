e007198029bd1da14be07039830241b8c7008fd9
andmari11 <andres.mari.piqueras@gmail.com>
2023-05-03 19:18:03 +0200

    a

--- ISCampusVirtual/tests/UsuarioTest.java
+++ ISCampusVirtual/tests/UsuarioTest.java
@@ -1,36 +1,34 @@
-package negocio;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import Negocio.Factoria.FactoriaSA;
 import Negocio.Usuario.SAUsuario;
 import Negocio.Usuario.TransferAlumno;
 import Negocio.Usuario.TransferUsuario;
 
-@RunWith(JUnit4.class)
-public class usuarioTest {
-  
+public class UsuarioTest {
+
   @Test
   public void test() {
-	SAUsuario saUsuario = FactoriaSA.getInstancia().generarSAUsuario();
-	TransferUsuario tU1= new TransferAlumno();
-	tU1.setId("s");
-	tU1.setPassword("s");
+    SAUsuario saUsuario = FactoriaSA.getInstancia().generarSAUsuario();
+    TransferUsuario tU1 = new TransferAlumno();
+    tU1.setId("s");
+    tU1.setPassword("s");
     saUsuario.crearUsuario(tU1);
     assertEquals(true, saUsuario.crearUsuario(tU1));
-    
-	TransferUsuario tU2= new TransferAlumno();
-	tU1.setId("a");
+
+    TransferUsuario tU2 = new TransferAlumno();
+    tU2.setId("a");
     saUsuario.crearUsuario(tU2);
     assertEquals(true, saUsuario.crearUsuario(tU2));
-    
-	TransferUsuario tU3= new TransferAlumno();
-	tU1.setId("s");
+
+    TransferUsuario tU3 = new TransferAlumno();
+    tU3.setId("s");
     saUsuario.crearUsuario(tU3);
     assertEquals(false, saUsuario.crearUsuario(tU3));
-    
+
     saUsuario.crearUsuario(tU3);
     assertEquals(true, saUsuario.eliminarUsuario(tU2.getId()));
     assertEquals(false, saUsuario.eliminarUsuario(tU2.getId()));
@@ -42,6 +40,5 @@ public class usuarioTest {
     assertEquals(true, saUsuario.editarUsuario(tU1));
     assertEquals(true, saUsuario.iniciarSesion(tU1.getId(), "a"));
     assertEquals(false, saUsuario.iniciarSesion(tU1.getId(), "s"));
-
   }
 }
